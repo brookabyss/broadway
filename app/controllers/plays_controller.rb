e7b75471caca4a37c17f96141de15d18a4bbd51d
Brook Kebede <brookabyss@gmail.com>
2016-09-22 23:55:31 +0000

    Plays model and Controller

---
+++ app/controllers/plays_controller.rb
@@ -0,0 +1,29 @@
+class PlaysController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+    
+  end
+   
+  def show
+  end
+
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  
+  private
+  
+  
+end
