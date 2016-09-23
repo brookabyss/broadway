24fa7986d332a1d6e4c70f5d7202c0a4101a617d
Brook Kebede <brookabyss@gmail.com>
2016-09-23 01:06:47 +0000

    Add New and create actions

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,16 +1,17 @@
 class PlaysController < ApplicationController
-  
+  before_action :play_params, only: [:create, :edit, :update, :destroy]
   def index
   end
   
   def new
-    
+    @play = Play.new
   end
    
   def show
   end
 
   def create
+    @play = Play.new(play_params)
   end
   
   def edit
@@ -25,7 +26,8 @@ class PlaysController < ApplicationController
   
   private
   
-  def find_play
+  def paly_params
+    params.require(:play).permit(:title, :description, :director)
   end
   
 end
