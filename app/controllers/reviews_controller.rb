222fc0ad704dd6837c748b07e25c17ead1198732
Brook Kebede <brookabyss@gmail.com>
2016-09-24 06:00:06 +0000

    Add edit and destroy functions to reviews and view pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :find_play
-  
+  before_action :find_review, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
   def new
     @review= Review.new
   end
@@ -9,13 +10,29 @@ class ReviewsController < ApplicationController
     @review= Review.new(review_params)
     @review.play_id= @play.id
     @review.user_id= current_user.id
-  
-  if @review.save
-    redirect_to play_path(@play)
-  else
-    render 'new'
+    
+    if @review.save
+      redirect_to play_path(@play)
+    else
+      render 'new'
+    end
+    
   end
   
+  def edit
+  end
+  
+  def update
+    if @review.update(review_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @review.destroy
+    redirect_to play_path(@play)
   end
   
   private
@@ -27,5 +44,8 @@ class ReviewsController < ApplicationController
   def find_play
     @play=Play.find(params[:play_id])
   end
-
+  
+  def find_review
+    @review= Review.find(params[:id])
+  end
 end
